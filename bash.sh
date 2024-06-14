4df15a3c5d97c4d95751de634d05e3fefd023d21
SaltyAUS <team1@saltyandcrew.au.eu.org>
2024-06-14 17:19:41 +1000

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -6,14 +6,11 @@ sudo apt install docker.io cmake snapd -y
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig 
 mkdir build
+cd build
 cmake ..
 make
 
-#gaining honey
 
-
-docker run honeygain/honeygain -tou-get
-docker run honeygain/honeygain -tou-accept -email ACCOUNT_EMAIL -pass ACCOUNT_PASSWORD -device DEVICE
 
 #myst nodes
 snap install hello-world
@@ -23,7 +20,11 @@ multipass launch
 
 sudo -E bash -c "$(curl -s https://raw.githubusercontent.com/mysteriumnetwork/node/master/install.sh)"
 
+#gaining honey
 
+
+sudo docker run honeygain/honeygain -tou-get
+sudo docker run honeygain/honeygain -tou-accept -email ACCOUNT_EMAIL -pass ACCOUNT_PASSWORD -device DEVICE
 
 print thanks end
 
